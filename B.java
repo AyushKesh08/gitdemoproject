e2d99f1948b30783abf5eadc543d401399cfea2c
Ayush Kesharwani <ayushkesharwani1708@gmail.com>
2026-08-22 10:07:56 +0530

    finally deleting the file B

--- B.java
+++
@@ -1,9 +0,0 @@
-class B{
-    public static void main(String[] args){
-        System.out.println("Hello World This Is File B....");
-
-        int x = 9898989898988;
-
-        System.out.println(x + "############");
-    }
-}
